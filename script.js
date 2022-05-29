748b49a8b88a3f3b3ed0e8ab8250d6bbfc55641c
kalevnakah <calebschmidt@gmail.com>
2022-05-29 17:41:33 -0500

    Updated file names. Worked on JavaScript objects undefined

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-var fs = require('file-system');
-var files = fs.readdirSync('./trails/');
-console.log(files);
+let trails = [];
 
 function extractData(file) {
+  const trail = {};
   var xhttp = new XMLHttpRequest();
-  //define the new trail
-  let trail = {};
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       //parse the JSON
       var response = JSON.parse(xhttp.responseText);
-      //Drill down to the trails properites
+      //Drill down to the trails properties
       var properties = response.features[0].properties;
-
-      document.getElementById('container').innerHTML = properties;
 
       trail.title = properties.title;
       trail.distance = properties.distance;
@@ -21,51 +16,52 @@ function extractData(file) {
       trail.average_speed = properties.average_speed;
       trail.notes = properties.notes;
 
-      console.log(trail);
+      trails = [...trails, trail];
     }
   };
   xhttp.open('GET', file, true);
   xhttp.send();
-  return trail;
-}
-
-let trails = [];
-
-function addTrail(file) {
-  const trail = extractData(file);
-  trails = [...trails, trail];
-  return trails;
 }
 
 //iterate through all the files and build the new trails list
-function compileTrails(fileNames) {
-  //  for each file in fileNames {
-  //    extractData(file);
-  //    append new trail to array
-  //  }
-  //  return trails;
+function compileTrails(numOfFiles) {
+  for (let i = 1; i <= numOfFiles; i++) {
+    let file = `trails/${i}.json`;
+    extractData(file);
+  }
 }
 
-addTrail('trails/1a-ch-52822-120413-pm.json');
-addTrail('trails/1b1-ch-52822-120301-pm.json');
+compileTrails(43);
 
-console.log(trails[0]);
+//find the total distance and time of route
+function routeTotal(route) {
+  console.log(route);
+  let totalDistance = route[0].distance;
+  //let totalTime = route[0].total_time;
+  //let averageSpeed = route[0].average_speed;
+  // let totalDistance = 0;
+  // let totalTime = 0;
+  // let totalSpeed = 0;
+  // for (let i in route) {
+  //   totalDistance += route[i].distance;
+  //   totalTime += route[i].total_time;
+  //   totalSpeed += route[i].average_speed;
+  // }
+  //let averageSpeed = totalSpeed / route.length;
+  //return [totalDistance, totalTime, averageSpeed];
+}
+
+//const trailTotals = routeTotal(trailsDir);
+//let distance = trails[0].title;
+console.log(trails);
+
+//console.log(JSON.stringify(trails[0]));
+//let trailString = ;
+//console.log(trails[0]);
+
+//document.getElementById('container').innerHTML = JSON.stringify(trails);
 
 // //Sudo Code
-
-// // extract useful data from JSON
-// function extractData(file)
-//   fetch the json
-//   parse the json
-//   create a new object
-//   populate new object with only the properties that I want
-//   return the new object
-
-// //create an array of all the json file names
-// function getFileNames{
-//   ???
-//   return array of file names
-// }
 
 // routeList = []
 // //recursively find all route patterns
